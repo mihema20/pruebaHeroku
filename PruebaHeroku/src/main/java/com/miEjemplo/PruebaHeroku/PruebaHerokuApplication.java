cdd2e33d589f07ab21e3f0c920503fa3e17d31ae
mihema20 <mihema20@gmail.com>
2022-07-05 17:25:29 +0200

    primer commit

---
+++ PruebaHeroku/src/main/java/com/miEjemplo/PruebaHeroku/PruebaHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.miEjemplo.PruebaHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaHerokuApplication.class, args);
+	}
+
+}
---
+++ PruebaHeroku/src/test/java/com/miEjemplo/PruebaHeroku/PruebaHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miEjemplo.PruebaHeroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
